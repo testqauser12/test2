755252d0e8f0b840ee9ad0520ce7a3023e0118b4
Natali <govtik_c@ukr.net>
2017-06-26 22:14:03 +0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,10 @@
+import unittest
+class Test1(unittest.TestCase):
+    def runTest(self):
+        print('test1 is passed')
+class Test2(unittest.TestCase):
+    def runTest(self):
+        print('test2 is passed')
+if __name__ == '__main__':
+    unittest.main()
+
